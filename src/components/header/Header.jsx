9d459961616cac5f2948bc2f4fa5a8a3dee0521b
Pratap Tyagi <tyagipratap111@gmail.com>
2021-05-18 21:58:57 +0530

    Created Header responsively

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,19 @@
 import "./Header.css";
+import { Schedule, HelpOutline, SearchOutlined } from "@material-ui/icons";
 
 function Header() {
   return (
-    <div>
-      <div>Hellog i am a header</div>
+    <div className="header">
+      <div className="header__left">
+        <Schedule />
+      </div>
+      <div className="header__search">
+        <SearchOutlined />
+        <input type="text" placeholder="Search" />
+      </div>
+      <div className="header__right">
+        <HelpOutline />
+      </div>
     </div>
   );
 }
