7c53769a81a2f36a23460c57719cf3de65c437aa
Pratap Tyagi <tyagipratap111@gmail.com>
2021-05-22 23:38:05 +0530

    Added google authentication

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,15 +1,25 @@
 import "./Login.css";
 import LOGO from "../../ascets/logo.png";
 import { Button } from "@material-ui/core";
+import { auth, provider } from "../../firebase";
 
 function Login() {
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => alert(err));
+  };
+
   return (
     <div className="login">
       <div className="login__container">
         <img src={LOGO} alt="LOGO" />
         <h1>Sign In</h1>
         <p>Slack</p>
-        <Button type="submit" className="button">
+        <Button type="submit" className="button" onClick={signIn}>
           Sign In
         </Button>
       </div>
